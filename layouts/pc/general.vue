06efa4ff3baed0613f86fd771ef3da8eae18b22a
momeemt <momiimt@gmail.com>
2020-04-07 17:17:42 +0900

    ✨ レイアウトを追加

---
+++ layouts/pc/general.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="app">
+    <v-app>
+      <Header page-type="General" />
+      <div id="flex">
+        <div id="leftContent">
+          <hyper-link-bar :listDatas="listDatas" pageType="General" />
+        </div>
+        <div id="middleContent">
+          <nuxt />
+        </div>
+        <div id="rightContent"></div>
+      </div>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import HyperLinkBar from '~/components/hyperLinkBar'
+import Header from '~/components/layouts/commonHeader'
+export default {
+  components: { HyperLinkBar, Header },
+  data() {
+    return {
+      user_id: /* this.$store.state.currentUser.user_id */ 16,
+      listDatas: [
+        { text: 'Home', icon: 'fas fa-home', link: '/' },
+        { text: '通知', icon: 'fas fa-bell', link: '/notification' },
+        { text: 'フィード', icon: 'far fa-comments', link: '/feed' },
+        { text: '便利リンク集', icon: 'fas fa-code-branch', link: '/links' },
+        { text: 'お知らせ', icon: 'fas fa-bullhorn', link: '/announce' },
+        {
+          text: 'プロフィール',
+          icon: 'far fa-id-card',
+          link: `/users/${this.user_id}`
+        },
+        {
+          text: 'プロフィールを編集',
+          icon: 'fas fa-user-edit',
+          link: '/settings/profile'
+        },
+        { text: '設定', icon: 'fas fa-cog', link: '/settings/home' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+#flex {
+  display: flex;
+}
+#leftContent {
+  width: 25%;
+  position: sticky;
+  top: 50px;
+  height: calc(100vh - 50px);
+  border-right: rgba(0, 0, 0, 0.12) solid 1px;
+}
+#middleContent {
+  width: 50%;
+}
+#rightContent {
+  width: 25%;
+  position: sticky;
+  top: 50px;
+  height: calc(100vh - 50px);
+  border-left: rgba(0, 0, 0, 0.12) solid 1px;
+}
+#flex {
+  display: flex;
+}
+#leftContent {
+  width: 25%;
+  position: sticky;
+  top: 50px;
+  height: calc(100vh - 50px);
+  border-right: rgba(0, 0, 0, 0.12) solid 1px;
+}
+#middleContent {
+  width: 50%;
+}
+#rightContent {
+  width: 25%;
+  position: sticky;
+  top: 50px;
+  height: calc(100vh - 50px);
+  border-left: rgba(0, 0, 0, 0.12) solid 1px;
+}
+</style>
---
+++ layouts/pc/manage.vue
@@ -0,0 +1,98 @@
+<template>
+  <div id="app">
+    <v-app>
+      <Header page-type="General" />
+      <div id="flex">
+        <div id="leftContent">
+          <hyper-link-bar :listDatas="listDatas" page-type="General" />
+        </div>
+        <div id="middleContent">
+          <nuxt />
+        </div>
+        <div id="rightContent"></div>
+      </div>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import HyperLinkBar from '~/components/hyperLinkBar'
+import Header from '~/components/layouts/commonHeader'
+export default {
+  components: { HyperLinkBar, Header },
+  data() {
+    return {
+      user_id: /* this.$store.state.currentUser.user_id */ 16,
+      listDatas: [
+        {
+          text: 'ダッシュボード',
+          icon: 'fas fa-tachometer-alt',
+          link: '/manages/dash'
+        },
+        { text: 'ユーザー管理', icon: 'fas fa-users', link: '/manages/users' },
+        {
+          text: 'お知らせ管理',
+          icon: 'fas fa-bullhorn',
+          link: '/manages/announces'
+        },
+        {
+          text: '通報管理',
+          icon: 'fas fa-exclamation-triangle',
+          link: `/manages/reports`
+        },
+        {
+          text: 'ドロップ管理',
+          icon: 'fas fa-eye-dropper',
+          link: '/manages/drops'
+        },
+        {
+          text: 'トピック管理',
+          icon: 'far fa-newspaper',
+          link: '/manages/topics'
+        },
+        {
+          text: '便利リンク管理',
+          icon: 'fas fa-code-branch',
+          link: '/manages/convenience_links'
+        },
+        {
+          text: 'Crafes! 管理',
+          icon: 'fas fa-crown',
+          link: `/manages/crafes`
+        },
+        {
+          text: 'Qrafters 管理',
+          icon: 'fas fa-question',
+          link: `/manages/qrafters`
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+#flex {
+  display: flex;
+}
+#leftContent {
+  width: 25%;
+  position: sticky;
+  top: 50px;
+  height: calc(100vh - 50px);
+  border-right: rgba(0, 0, 0, 0.12) solid 1px;
+}
+#middleContent {
+  width: 50%;
+}
+#rightContent {
+  width: 25%;
+  position: sticky;
+  top: 50px;
+  height: calc(100vh - 50px);
+  border-left: rgba(0, 0, 0, 0.12) solid 1px;
+}
+#flex {
+  display: flex;
+}
+</style>

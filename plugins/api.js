ea7c3f053e396c18b39ccd8b3ee713a908f7ad82
momeemt <momiimt@gmail.com>
2020-04-18 20:48:43 +0900

    ♻️ APIをCRUDで共通化

--- plugins/api.js
+++ plugins/api.js
@@ -1,92 +1,57 @@
 export default ({ app }, inject) => {
   const api = {
-    Drops: {
-      async index() {
-        const drops = await app.$axios.$get('/api/v1/drops').catch((err) => {
-          console.log(err)
+    async index(modelName) {
+      let resCode
+      const res = await app.$axios
+        .$get(`/api/v1/${modelName}`)
+        .then((res) => {
+          resCode = 200
+          return res
         })
-        return { drops }
-      },
-      async create(data) {
-        await app.$axios.$post('/api/v1/drops', data).catch((err) => {
-          console.log(err)
+        .catch((err) => {
+          resCode = err.response.status
         })
-      },
-      async delete(data) {
-        await app.$axios.$delete(`/api/v1/drops/${data.id}`, data)
-      },
-      async show(id) {
-        const drop = await app.$axios
-          .$get(`/api/v1/drops/${id}`)
-          .catch((err) => {
-            console.log(err)
-          })
-        return { drop }
-      }
+      return { res, resCode }
     },
-    Users: {
-      async index() {
-        const users = await app.$axios.$get('/api/v1/users').catch((err) => {
-          console.log(err)
+    async create(modelName, data) {
+      await app.$axios
+        .$post(`/api/v1/${modelName}`, data)
+        .then((res) => {
+          return res
         })
-        return { users }
-      },
-      async create(data) {
-        await app.$axios.$post('/api/v1/users', data).catch((err) => {
-          console.log(err)
+        .catch((err) => {
+          return err
         })
-      },
-      async show(id) {
-        const user = await app.$axios
-          .$get(`/api/v1/users/${id}`)
-          .catch((err) => {
-            console.log(err)
-          })
-        return { user }
-      },
-      async getByUserId(userId) {
-        const user = await app.$axios
-          .$get(`/api/v1/users/user_id/${userId}`)
-          .catch((err) => {
-            console.log(err)
-          })
-        return { user }
-      }
     },
-    ConvenienceLinks: {
-      async index() {
-        const convenienceLink = await app.$axios
-          .$get('/api/v1/convenience_links')
-          .catch((err) => {
-            console.log(err)
-          })
-        return { convenienceLink }
-      },
-      async create(data) {
-        await app.$axios
-          .$post('/api/v1/convenience_links', data)
-          .catch((err) => {
-            console.log(err)
-          })
-      },
-      async update(data) {
-        await app.$axios
-          .$put(`/api/v1/convenience_links/${data.id}/edit`, data)
-          .catch((err) => {
-            console.log(err)
-          })
-      },
-      async delete(data) {
-        await app.$axios.$delete(`/api/v1/convenience_links/${data.id}`, data)
-      }
-    },
-    News: {
-      async index() {
-        const drops = await app.$axios.$get('/api/v1/news').catch((err) => {
-          console.log(err)
+    async delete(modelName, data) {
+      await app.$axios
+        .$delete(`/api/v1/${modelName}/${data.id}`, data)
+        .then((res) => {
+          return res
         })
-        return { drops }
-      }
+        .catch((err) => {
+          return err
+        })
+    },
+    async show(modelName, id) {
+      await app.$axios
+        .$get(`/api/v1/${modelName}/${id}`)
+        .then((res) => {
+          return res
+        })
+        .catch((err) => {
+          return err
+        })
+    },
+    async update(modelName, data) {
+      await app.$axios
+        .$put(`/api/v1/${modelName}/${data.id}/edit`, data)
+        .then((res) => {
+          return res
+        })
+        .catch((err) => {
+          return err
+        })
     }
   }
   inject('api', api)

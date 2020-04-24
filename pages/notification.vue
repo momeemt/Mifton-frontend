b09d1cd23381ffd4a7238e6520375bace3be364c
momeemt <momiimt@gmail.com>
2020-04-24 10:51:53 +0900

    ✨ 通知ページが通知を読み込むようにした

--- pages/notification.vue
+++ pages/notification.vue
@@ -1,13 +1,24 @@
 <template>
   <div>
     <h1>通知</h1>
+    <v-list>
+      <v-list-item v-for="notice in notificationsData">
+        <v-list-item-content>{{ notice.content }}</v-list-item-content>
+      </v-list-item>
+    </v-list>
   </div>
 </template>
 
 <script>
 export default {
   name: 'NotificationVue',
-  layout: 'pc/general'
+  layout: 'pc/general',
+  async asyncData({ app }) {
+    const res = await app.$api.index('notifications')
+    const notificationsData = res.res
+    const resCode = res.resCode
+    return { notificationsData, resCode }
+  }
 }
 </script>
 

e388c8c4b9b0a0af3b9651526a1c025c2bcaaafd
momeemt <momiimt@gmail.com>
2020-04-27 11:17:05 +0900

    ✨ ログインページでAPIを使うように書き直し、エラーハンドリング

--- pages/login.vue
+++ pages/login.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="loginPage">
-    <Header pageType="General" />
+    <Header page-type="General" />
     <div>
       <p>ログインする</p>
       <v-text-field v-model="user_id" placeholder="ユーザーID"></v-text-field>
-      <v-text-field v-model="password" placeholder="パスワード"></v-text-field>
+      <v-text-field
+        v-model="password"
+        type="password"
+        placeholder="パスワード"
+      ></v-text-field>
       <v-btn @click="login">送信</v-btn>
-      <v-btn @click="logout">ログアウト</v-btn>
+      <v-alert v-show="unAuthorizedError" dense outlined type="error">
+        メールアドレス認証が完了していません。メールボックスを確認してください。
+      </v-alert>
+      <v-alert v-show="unprocessableEntityError" dense outlined type="error">
+        メールアドレスかパスワードが間違っています。もう一度確認してください。
+      </v-alert>
     </div>
     <Footer></Footer>
   </div>
@@ -24,24 +33,31 @@ export default {
   data() {
     return {
       user_id: '',
-      password: ''
+      password: '',
+      unAuthorizedError: false,
+      unprocessableEntityError: false
     }
   },
   methods: {
-    login() {
-      this.$axios
-        .$post('/api/v1/sessions', {
-          user_id: this.user_id,
-          password: this.password
-        })
-        .then((res) => {
-          console.log(res)
-          this.$store.commit('login', res)
-          this.$router.push('/')
-        })
-        .catch((err) => {
-          console.log(err)
-        })
+    async login() {
+      this.unAuthorizedError = false
+      this.unprocessableEntityError = false
+      const res = await this.$api.create('sessions', {
+        user_id: this.user_id,
+        password: this.password
+      })
+      const resData = res.res
+      const resCode = res.resCode
+      if (resCode === 200) {
+        this.$store.commit('login', resData)
+        this.$router.push('/')
+      } else if (resCode === 401) {
+        // 認証なし
+        this.unAuthorizedError = true
+      } else {
+        // 間違ってるよ
+        this.unprocessableEntityError = true
+      }
     },
     logout() {
       this.$store.commit('logout')

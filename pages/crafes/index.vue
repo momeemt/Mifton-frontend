10666d9cfe59e4ca19a1a7b7abf3f86093a9c731
momeemt <momiimt@gmail.com>
2020-04-29 15:43:20 +0900

    ✨ Crafes!とQraftersのトップページを実装

---
+++ pages/crafes/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Header page-type="Crafes" />
+    <v-img
+      class="white--text align-end"
+      height="500px"
+      src="https://mifton.xyz/crafes-top.jpg"
+    >
+      <v-row align="end">
+        <v-col>
+          <h1>Crafes!</h1>
+          <h1>技術で新しい世界を組み立てよう</h1>
+        </v-col>
+        <v-col>
+          <LoginCard />
+        </v-col>
+      </v-row>
+    </v-img>
+  </div>
+</template>
+
+<script>
+import Header from '../../components/Layouts/Header'
+import LoginCard from '~/components/Organisms/LoginCard'
+export default {
+  name: 'CrafesIndex',
+  components: {
+    Header,
+    LoginCard
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ pages/qrafters/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <Header page-type="Qrafters" />
+    <v-img class="white--text align-end" height="500px" src="iceland.png">
+      <v-row align="end">
+        <v-col>
+          <h1>Qrafters</h1>
+          <h1>疑問を一気に解決しよう</h1>
+        </v-col>
+        <v-col>
+          <LoginCard />
+        </v-col>
+      </v-row>
+    </v-img>
+    <div>
+      <h2>現在開発中！</h2>
+    </div>
+    <div>
+      <h2>疑問を解決するサービス、Qrafters</h2>
+      <p>
+        Minecraftを遊んでいて、疑問を抱えることは多々あると思います。
+      </p>
+      <p>
+        例えば、コマンドならexecuteコマンドや、アドオンならアニメーションコントローラーなど、難しい仕組みは多くあります。
+      </p>
+      <p>
+        誰かに聞きたいけれど誰に聞けばわからない...。それを解決します。
+      </p>
+      <p>
+        Qraftersでは、まず同様の質問がされているか検索して、なければ質問することができます。
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from '../../components/Layouts/Header'
+import LoginCard from '~/components/Organisms/LoginCard'
+export default {
+  name: 'QraftersIndex',
+  components: {
+    Header,
+    LoginCard
+  }
+}
+</script>
+
+<style scoped></style>

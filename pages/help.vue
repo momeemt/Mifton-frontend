3f6fa5529575c4de7ad40b1bedd94ff78f3e3b0c
momeemt <momiimt@gmail.com>
2020-04-29 15:43:33 +0900

    ✨ ヘルプページを実装

---
+++ pages/help.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Header page-type="General" />
+    <h1>ヘルプページ</h1>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '../components/Layouts/Header'
+import Footer from '../components/Layouts/Footer'
+export default {
+  name: 'HelpVue',
+  components: { Footer, Header }
+}
+</script>
+
+<style scoped></style>

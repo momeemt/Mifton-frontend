94f70b04cfa5a6de61e5322fb0fae9b78e0ecc21
momeemt <momiimt@gmail.com>
2020-05-11 14:54:05 +0900

    ✨ 認証リダイレクトの追加 チラッと映る未ログインページを回避

--- middleware/homeRedirectLoggedUser.js
+++ middleware/homeRedirectLoggedUser.js
@@ -1,8 +1,11 @@
 export default function({ store }) {
-  if (store.getters.is_logged_in) {
+  console.error(store.getters.is_logged_in)
+  if (!store.getters.is_logged_in) {
+    console.log('guest')
     return Promise.resolve()
   } else if (process.browser) {
-    window.location.href = '/bector'
+    window.location.href = '/home'
+    console.log('logged')
     return new Promise((resolve) => {
       // レンダリング完了まで待つ
     })
--- middleware/topRedirectGuest.js
+++ middleware/topRedirectGuest.js
@@ -1,8 +1,9 @@
 export default function({ store }) {
+  console.error(store.getters.is_logged_in)
   if (store.getters.is_logged_in) {
     return Promise.resolve()
   } else if (process.browser) {
-    window.location.href = '/bector'
+    window.location.href = '/'
     return new Promise((resolve) => {
       // レンダリング完了まで待つ
     })

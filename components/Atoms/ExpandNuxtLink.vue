60cdaddfe4c5a3c019ddd7a36e861eb3fe8696a6
Momeemt <momiimt@gmail.com>
2020-05-25 22:34:04 +0900

    ✂️ 内部リンクと外部リンクの拡張Ccomponent

---
+++ components/Atoms/ExpandNuxtLink.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="expandNuxtLink">
+    <i :class="iconClass"></i>
+    <nuxt-link v-if="condition" :to="trueLink">{{ showContents }}</nuxt-link>
+    <nuxt-link v-else :to="falseLink">{{ showContents }}</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    iconClass: {
+      type: String,
+      required: true
+    },
+    condition: {
+      type: Boolean,
+      required: true
+    },
+    trueLink: {
+      type: String,
+      required: true
+    },
+    falseLink: {
+      type: String,
+      required: true
+    },
+    showContents: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.expandNuxtLink {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  p {
+    margin: 0;
+  }
+  a {
+    text-decoration: none;
+    color: white;
+  }
+  i {
+    padding-right: 0.5em;
+  }
+}
+</style>
---
+++ components/Atoms/ExternalLink.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="externalLink">
+    <i :class="iconClass"></i>
+    <a :href="href" target="_blank" rel="noopener">{{ showContents }}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      required: true
+    },
+    showContents: {
+      type: String,
+      required: true
+    },
+    iconClass: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.externalLink {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  p {
+    margin: 0;
+  }
+  a {
+    text-decoration: none;
+    color: white;
+  }
+  i {
+    padding-right: 0.5em;
+  }
+}
+</style>
